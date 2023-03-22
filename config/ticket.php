fe8ba33aefc8d8c4cbbbe721fd94e36e3d717b3a
Felice <felice.ostuni@uania.com>
2023-03-22 16:50:48 +0100

    wip

---
+++ config/ticket.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'opening_hours'=> [
+        'monday' => ['09:00-13:00', '14:00-18:00'],
+        'tuesday' => ['09:00-13:00', '14:00-18:00'],
+        'wednesday' => ['09:00-13:00', '14:00-18:00'],
+        'thursday' => ['09:00-13:00', '14:00-18:00'],
+        'friday' => ['09:00-13:00', '14:00-18:00'],
+        'saturday' => [],
+        'sunday' => [],
+        'holidaysAreClosed' => true,
+        'holidays' => [
+            'region' => 'it-national',
+            'with' => [
+                'company-special-holiday' => '12-24'
+            ],
+        ],
+    ]
+];
