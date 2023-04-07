b4c8f682e56fbfe29299c6ad33db957b512c0583
Felice <felice.ostuni@uania.com>
2023-04-07 08:46:19 +0200

    wip

--- tests/Feature/TicketsTest.php
+++ tests/Feature/TicketsTest.php
@@ -34,46 +34,46 @@ class TicketsTest extends TestCase
 //        $this->user = User::find('22222222-2222-0000-2222-222222222222');
 //        $this->admin = User::find('11111111-1111-0000-1111-111111111111');
     }
-
-    public function test_can_see_livewire_component_on_tickets_page()
-    {
-        $this->actingAs($this->admin)
-            ->withoutExceptionHandling()
-            ->get(route_lang('tickets.tickets.table'))
-            ->assertSuccessful()
-            ->assertSeeLivewire('ticket::tickets-tickets-table');
-    }
-
-    public function test_admin_can_reply_to_tickets()
-    {
-        //isp crea ticket
-        Livewire::actingAs($this->isp)
-            ->test('ticket::tickets-isp-tickets-table')
-            ->set('subject', 'Oggetto ticket')
-            ->set('content', 'Contenuto ticket')
-            ->set('ticket_category_id', 1)//technical
-            ->call('newTicket')
-            ->assertRedirect(route_lang('tickets.tickets.table'));
-
-        $ticket = Ticket::where('subject','=','Oggetto ticket')->first();
-
-        Model::clearBootedModels();
-
-        //l'admin lo vede nell'elenco
-        Livewire::actingAs($this->admin)
-            ->test('ticket::tickets-tickets-table')
-            ->assertSee('Oggetto ticket')
-        ;
-
-        //può assegnarselo e rispondere
-        Livewire::actingAs($this->admin)
-            ->test('ticket::tickets-tickets-view',['ticket'=> $ticket])
-            ->set('agent_id', $this->admin->id)
-            ->call('assign')
-            ->set('content', 'ticket comment!')
-            ->call('comment')
-            //->assertEmitted('livewire-on-messages', 'nuova risposta creata')
-        ;
-
-    }
+//
+//    public function test_can_see_livewire_component_on_tickets_page()
+//    {
+//        $this->actingAs($this->admin)
+//            ->withoutExceptionHandling()
+//            ->get(route_lang('tickets.tickets.table'))
+//            ->assertSuccessful()
+//            ->assertSeeLivewire('ticket::tickets-tickets-table');
+//    }
+//
+//    public function test_admin_can_reply_to_tickets()
+//    {
+//        //isp crea ticket
+//        Livewire::actingAs($this->isp)
+//            ->test('ticket::tickets-isp-tickets-table')
+//            ->set('subject', 'Oggetto ticket')
+//            ->set('content', 'Contenuto ticket')
+//            ->set('ticket_category_id', 1)//technical
+//            ->call('newTicket')
+//            ->assertRedirect(route_lang('tickets.tickets.table'));
+//
+//        $ticket = Ticket::where('subject','=','Oggetto ticket')->first();
+//
+//        Model::clearBootedModels();
+//
+//        //l'admin lo vede nell'elenco
+//        Livewire::actingAs($this->admin)
+//            ->test('ticket::tickets-tickets-table')
+//            ->assertSee('Oggetto ticket')
+//        ;
+//
+//        //può assegnarselo e rispondere
+//        Livewire::actingAs($this->admin)
+//            ->test('ticket::tickets-tickets-view',['ticket'=> $ticket])
+//            ->set('agent_id', $this->admin->id)
+//            ->call('assign')
+//            ->set('content', 'ticket comment!')
+//            ->call('comment')
+//            //->assertEmitted('livewire-on-messages', 'nuova risposta creata')
+//        ;
+//
+//    }
 }
